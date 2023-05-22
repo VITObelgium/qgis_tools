39b0aade72f2025121f201476b17c234ae5a1344
Thomas Danckaert <thomas.danckaert@vito.be>
2023-05-22 16:19:32 +0200

    sitecustomize.py: Use sys.base_prefix to find OSGeo4W Python dir.
    This is more robust than using the numpy package __file__ attribute.

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,24 +1,30 @@
 """Set required environment variables for GDAL, proj4 bundled with OSGeo4W.
  
-We assume 'import numpy' imports the numpy package bundled with OSGeo4W, in order to find the OSGeo4W root path.
+We assume an environment where sys.base_prefix points to the Python interpreter in %OSGEO4W_ROOT%\apps\Pythonxy.
 """
  
 import os
+import sys
 from pathlib import Path
  
-import numpy as np
+python_root = sys.base_prefix
+osgeo4w_root = str(Path(python_root).parents[1])
 
-osgeo4w_root = str(Path(np.__file__).parents[5])
- 
+os.environ['OSGEO4W_ROOT'] = osgeo4w_root
 _envvars = {
-    'GDAL_DATA': r'share\gdal',
-    'GDAL_DRIVER_PATH': r'bin\gdalplugins',
-    'GEOTIFF_CSV': r'share\epsg_csv',
-    'PROJ_LIB': r'share\proj',
+    'GDAL_DATA': os.path.join('share', 'gdal'),
+    'GDAL_DRIVER_PATH': os.path.join('bin', 'gdalplugins'),
+    'GEOTIFF_CSV': os.path.join('share','epsg_csv'),
+    'PROJ_LIB': os.path.join('share','proj'),
 }
- 
+
 for var, rel_path in _envvars.items():
     os.environ[var] = os.path.join(osgeo4w_root, rel_path)
- 
-os.environ['PATH'] =  os.path.join(osgeo4w_root, 'bin') + ';' + os.environ['PATH']
+
+sys.path.append(os.path.join(python_root, 'Scripts'))
+
+os.environ['PATH'] = ';'.join([os.environ['PATH'],
+                               os.path.join(osgeo4w_root, 'bin'),
+                               os.path.join(python_root, 'Scripts')])
+
 os.add_dll_directory(os.path.join(osgeo4w_root, 'bin'))
